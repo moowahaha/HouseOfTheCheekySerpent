2e43ce9361a159d1d5f7c5d00730029700c731f5
Stephen Hardisty <shardisty@etsy.com>
2013-11-23 21:18:58 -0500

    lil code cleanup

--- replace_crossfit.js
+++ replace_crossfit.js
@@ -1,10 +1,12 @@
+var matcher = /\bcross\s*fit\b/gi;
+
 function replaceCrossFit() {
-    if (document.body.innerText.match(/cross\s*fit/i)) {
+    if (document.body.innerText.match(matcher)) {
         for (i = 0; i < document.all.length; i++) {
             var child = document.all(i).firstChild;
 
             if (child && child.data && child.tagName != 'INPUT' && child.tagName != 'TEXTAREA') {
-                child.data = child.data.replace(/cross\s*fit/gi, 'House of The Cheeky Serpent');
+                child.data = child.data.replace(matcher, 'House of The Cheeky Serpent');
             }
         }
     }
